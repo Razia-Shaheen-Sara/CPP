949d8bc73c2c7b1bdd2a8c88de01751645e75926
Razia-Shaheen-Sara <147872964+Razia-Shaheen-Sara@users.noreply.github.com>
2025-02-25 15:20:45 +0100

    Add files via upload

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		int i = 1;
+		int j;
+		while (argv[i])
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout <<(char)toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
